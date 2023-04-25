339785c61a04648cf88bbdec87420d49a1344c7a
Leon Patmore <44168466+LeonPatmore@users.noreply.github.com>
2023-04-25 20:04:06 +0100

    Adding on connect request template.

--- src/lambda-handlers/onConnect.js
+++ src/lambda-handlers/onConnect.js
@@ -1,3 +1,4 @@
-exports.handler = async () => {
+exports.handler = async (event) => {
+    console.log(`Processing on connect event [ ${JSON.stringify(event)} ]`);
     return { statusCode: 200, body: 'Connected.' };
 };
